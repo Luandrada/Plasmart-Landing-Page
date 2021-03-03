8def687960ca44159a5d0187a1c61eeadf330c40
Luandrada <luandrada12@live.com>
2021-03-03 16:49:31 -0300

    seccion productos

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function abrirGaleria(){
+document.getElementById("bg-galeria").style.display="block"
+}
+function cerrarGaleria() {
+    document.getElementById("bg-galeria").style.display="none"
+
+}
